64e25377fb0774d236baee2e0b7ad65d61d1eed7
sebastianbastian12 <sebastian.oliveros50@gmail.com>
2020-10-01 22:49:02 -0500

    initial commit

---
+++ Lesson3/plan.js
@@ -0,0 +1,36 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+let d = new Date();
+let string = document.lastModified;
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = "Current Date:" + " " + dayName + ", " + "  " + monthName + "," + "  " + d.getDate() + "," + "  " + year + ".";
+let lupdated = "Last Update:" + " " + string + ".";
+document.getElementById("currentdate").textContent = fulldate;
+document.getElementById("lastupdated").textContent = lupdated;
+d.getDay()
+d.getMonth()
+d.getFullYear()
+d.getDate()
