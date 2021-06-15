404033dbf7b2e86821e5a6eefcdeb1cc59089684
sebastianbastian12 <sebastian.oliveros50@gmail.com>
2021-06-15 18:31:53 -0500

    initial commits

---
+++ Lesson9/prophets.js
@@ -0,0 +1,34 @@
+1
+const source = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+fetch(source)
+    .then(data => data.json())
+    .then(data2 => {
+        const prophets = data2['prophets'];
+        console.table(prophets);
+
+        for (let i = 0; i < prophets.length; i++) {
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let h3 = document.createElement('h3');
+            let h4 = document.createElement('h3');
+            let img = document.createElement('img');
+
+
+            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            h3.textContent = prophets[i].birthdate;
+            h4.textContent = prophets[i].birthplace;
+            img.setAttribute('src', prophets[i].imageurl)
+            img.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + '-' + (i + 1))
+
+
+            card.appendChild(h2);
+            card.appendChild(h3);
+            card.appendChild(h4);
+            card.appendChild(img);
+
+
+
+            document.querySelector('div.cards').appendChild(card);
+
+        }
+    })
